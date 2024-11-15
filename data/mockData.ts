7092ad598fa61e3c979127b6a6ee2e7a8580c95a
Haoze <haoze04@gmail.com>
2024-11-15 07:03:57 +0000

    insert sample airport data

---
+++ data/mockData.ts
@@ -0,0 +1,30 @@
+export const airportData = [
+    {
+      AirportId: 3411,
+      AirportName: "Barter Island LRRS Airport",
+      IATACode: "BTI",
+      ICAOCode: "PABA",
+      Longitude: -143.5820007,
+      Latitude: 70.13400269,
+      CityId: 82,
+    },
+    {
+      AirportId: 3412,
+      AirportName: "Wainwright Air Station",
+      IATACode: "NA",
+      ICAOCode: "PAWT",
+      Longitude: -159.8600006,
+      Latitude: 70.61340332,
+      CityId: 394,
+    },
+    {
+      AirportId: 3413,
+      AirportName: "Cape Lisburne LRRS Airport",
+      IATACode: "LUR",
+      ICAOCode: "PALU",
+      Longitude: -166.1100006,
+      Latitude: 68.87509918,
+      CityId: 164,
+    },
+  ];
+  
